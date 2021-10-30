bd1634abe4bf73957e5c02545525342016f98d11
Abhishek <abhishekdav18@gmail.com>
2021-10-30 14:37:50 +0530

    Third Commit

--- src/test/java/Tests/SmokeTest.java
+++ src/test/java/Tests/SmokeTest.java
@@ -4,7 +4,9 @@ public class SmokeTest {
 
 	public static void main(String[] args) {
 	System.out.println("Hello People");
-
+	System.out.println("Hello People");
+	System.out.println("Hello People");
+	System.out.println("Hello People");
 	}
 
 }

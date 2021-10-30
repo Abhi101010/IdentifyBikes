b2129babc645d3fd5d76ce149ac242d1ccc353bd
Abhishek <abhishekdav18@gmail.com>
2021-10-30 14:52:43 +0530

    Last

--- src/test/java/Tests/SmokeTest.java
+++ src/test/java/Tests/SmokeTest.java
@@ -3,6 +3,9 @@ package Tests;
 public class SmokeTest {
 
 	public static void main(String[] args) {
+	System.out.println("Hello ");
+	System.out.println("Hello ");
+	System.out.println("Hello ");
 	
 	}
 
